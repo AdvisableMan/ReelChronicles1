03ecb883cf7b8b308d779fc849bd7a2f707e9105
advisableman <gandolaandrea@gmail.com>
2019-10-20 00:46:29 +0200

    [RC1][CODE][Character][RootMotion] Setting up new character for root motion.

--- Source/Animations/AnimationsGameMode.cpp
+++ Source/Animations/AnimationsGameMode.cpp
@@ -8,6 +8,8 @@ AAnimationsGameMode::AAnimationsGameMode()
 {
 	// set default pawn class to our Blueprinted character
 	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT(" /Game/Content/AZRI/Blueprints/ThirdPersonCharacter"));
+	//(TEXT(" /Game/Content/AZRI/Blueprints/ThirdPersonCharacter"));
+	//(TEXT(" /Game/Content/AZRI/AZRI_BP"));
 	if (PlayerPawnBPClass.Class != NULL)
 	{
 		DefaultPawnClass = PlayerPawnBPClass.Class;
